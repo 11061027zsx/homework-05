1a8b9d165ed76ba327e3b634624b7281e9de4a98
11061027zsx <lightz_@outlook.com>
2013-11-04 13:12:04 +0800

    homework-05

---
+++ C2/C2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace C2
+{
+
+
+    class saaaa
+    {
+        public static void Main()
+        {
+
+
+            client[] a =new client[20];
+            for (int i = 0; i < 20; i++)
+            {
+                a[i] = new client { sname = "c-" + i.ToString(), scode = "123" };
+                a[i].connnectserver();
+                a[i].Sign_Receive();
+                a[i].Regester();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+    }
+
+
